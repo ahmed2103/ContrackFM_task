160b5573c0ed97ca9ccd6bc0fa04abef007743b0
Hasona <bekoahmed20@gmail.com>
2025-12-23 22:22:15 +0200

    fixing class name

---
+++ CompanyAPI/DTOs/CustomerDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CompanyAPI.DTOs
+{
+    public class CustomerDTO
+    {
+        public string CustomerId { get; set; }
+        public string CustomerName { get; set; }
+
+        public List<CustomerOrderDTO> Orders { get; set; }
+    }
+}
---
+++ CompanyAPI/DTOs/DepartmentSalarySumDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CompanyAPI.DTOs
+{
+    public class DepartmentSalarySumDTO
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public decimal SalarySum { get; set; }
+    }
+}
